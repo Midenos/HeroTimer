56b61a60ec968c1717e3169bd4b0338624264a0f
Nico Diestelmann <53916946+Midenos@users.noreply.github.com>
2022-03-26 01:57:05 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from ui import GUI_Setup
 import pandas as pd
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import *
 
 # -----GLOBALS-----
 FILEPATH = None
 DATA = None
+OPTION = 0
 
 
 # -----FUNCTIONS-----
@@ -17,8 +19,30 @@ def open_file_for_pd():
     try:
         DATA = pd.read_excel(FILEPATH)
     except:
+        FILEPATH = None
         messagebox.showerror("Excel Error", "File Error, check excel data")
     else:
+        calculate_worktime()
+
+
+def set_options():
+    global OPTION
+    if cb_all_var.get() == 1:
+        OPTION = 0
+        cb_bes_var.set(1)
+        cb_pen_var.set(1)
+    elif cb_pen_var.get() == 1 and cb_bes_var.get() == 1:
+        OPTION = 3
+        cb_all_var.set(0)
+    elif cb_bes_var.get() == 1:
+        OPTION = 1
+        cb_all_var.set(0)
+    elif cb_pen_var.get() == 1:
+        OPTION = 2
+        cb_all_var.set(0)
+    else:
+        OPTION = -1
+    if FILEPATH is not None:
         calculate_worktime()
 
 
@@ -27,12 +51,35 @@ def calculate_worktime():
     minutes = 0
     seconds = 0
     try:
-        for time in DATA["Arbeitszeit"]:
-            hours += int(time[0:2])
-            minutes += int(time[3:5])
-            seconds += int(time[6:8])
-        minutes += int(seconds/60)
-        hours += int(minutes/60)
+        if OPTION == -1:
+            gui.worktime_label.config(text="00:00:00")
+            gui.path_label.config(text="In Bearbeitung: " + FILEPATH)
+            return
+        if OPTION == 0:
+            for time in DATA["Arbeitszeit"]:
+                hours += int(time[0:2])
+                minutes += int(time[3:5])
+                seconds += int(time[6:8])
+        elif OPTION == 1:
+            for index, row in DATA.iterrows():
+                if row["Status"] == "Bestätigt":
+                    hours += int(row["Arbeitszeit"][0:2])
+                    minutes += int(row["Arbeitszeit"][3:5])
+                    seconds += int(row["Arbeitszeit"][6:8])
+        elif OPTION == 2:
+            for index, row in DATA.iterrows():
+                if row["Status"] == "Eingereicht":
+                    hours += int(row["Arbeitszeit"][0:2])
+                    minutes += int(row["Arbeitszeit"][3:5])
+                    seconds += int(row["Arbeitszeit"][6:8])
+        elif OPTION == 3:
+            for index, row in DATA.iterrows():
+                if row["Status"] == "Bestätigt" or row["Status"] == "Eingereicht":
+                    hours += int(row["Arbeitszeit"][0:2])
+                    minutes += int(row["Arbeitszeit"][3:5])
+                    seconds += int(row["Arbeitszeit"][6:8])
+        minutes += int(seconds / 60)
+        hours += int(minutes / 60)
         minutes = minutes % 60
         seconds = seconds % 60
     except:
@@ -40,10 +87,19 @@ def calculate_worktime():
         gui.path_label.config(text="ERROR with: " + FILEPATH)
     else:
         gui.worktime_label.config(text=str(hours) + ":" + str(minutes).zfill(2) + ":" + str(seconds).zfill(2))
-        gui.path_label.config(text=FILEPATH)
+        gui.path_label.config(text="In Bearbeitung: " + FILEPATH)
 
 
 # -----MAIN-----
 gui = GUI_Setup()
+cb_all_var = IntVar()
+cb_bes_var = IntVar()
+cb_pen_var = IntVar()
 gui.open_file_btn.config(command=lambda: open_file_for_pd())
+gui.all_cb.config(variable=cb_all_var, command=lambda: set_options())
+gui.bes_cb.config(variable=cb_bes_var, command=lambda: set_options())
+gui.pen_cb.config(variable=cb_pen_var, command=lambda: set_options())
+cb_all_var.set(0)
+cb_pen_var.set(1)
+cb_bes_var.set(1)
 gui.root.mainloop()
--- ui.py
+++ ui.py
@@ -20,17 +20,34 @@ class GUI_Setup:
         self.open_file_btn.config(text="Open File", width=30)
         self.open_file_btn.grid(column=0, row=1, pady=20)
 
+        self.state_label = Label()
+        self.state_label.config(text="Status Option", pady=5, bg=THEME_COLOR, font=("Arial", 10), fg="#ffdd00")
+        self.state_label.grid(column=0, row=2, sticky="W")
+
+        self.all_cb = Checkbutton()
+        self.all_cb.config(text="Alle inkl. gelöscht & vorläufig", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.all_cb.grid(column=0, row=3, pady=5, sticky="W")
+
+        self.bes_cb = Checkbutton()
+        self.bes_cb.config(text="Bestätigt", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.bes_cb.grid(column=0, row=4, padx=00, sticky="W")
+
+        self.pen_cb = Checkbutton()
+        self.pen_cb.config(text="Eingereicht", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.pen_cb.grid(column=0, row=4, padx=95, sticky="W")
+
         self.worktime_info_label = Label()
         self.worktime_info_label.config(text="Arbeitszeit in Stunden:Minuten:Sekunden",
                                         pady=10, bg=THEME_COLOR, font=("Arial", 15), fg="#ffdd00")
-        self.worktime_info_label.grid(column=0, row=2)
+        self.worktime_info_label.grid(column=0, row=5)
 
         self.worktime_label = Label()
         self.worktime_label.config(text="00:00:00", pady=5, bg=THEME_COLOR, font=("Arial", 15))
-        self.worktime_label.grid(column=0, row=3)
+        self.worktime_label.grid(column=0, row=6)
 
         self.path_label = Label()
         self.path_label.config(text="", pady=5, bg=THEME_COLOR, font=("Arial", 10))
-        self.path_label.grid(column=0, row=4)
+        self.path_label.grid(column=0, row=7)
+
 
 
