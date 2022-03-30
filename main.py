69ce195615a678e8b871110e2919a73c4f254b94
Nico Diestelmann <53916946+Midenos@users.noreply.github.com>
2022-03-30 23:03:28 +0200

    Update Status & Kategorie
    Neue Status und Kategorie Optionen

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from tkinter import *
 # -----GLOBALS-----
 FILEPATH = None
 DATA = None
-OPTION = 3
+# CATEGORIE = ["Umsetzung", "Büro", "Homeoffice", "Schlechtwetter", "Materialfahrt", "Aufgabe", "Fahrtzeit",
+#              "Projektierung", "Vor-Ort-Termin", "Besprechung", "Pause", "Privat", "Keine"]
+CATEGORIE = ["Umsetzung", "Aufgabe", "Projektierung", "Vor-Ort-Termin", "Keine"]
+STATUS = ["Eingereicht", "Bestätigt"]
 
 
 # -----FUNCTIONS-----
 def open_file_for_pd():
     global FILEPATH
     global DATA
-    FILEPATH = filedialog.askopenfilename(initialdir="/", title="Wählen Sie die CSV",
+    FILEPATH = filedialog.askopenfilename(initialdir="/", title="Wählen Sie die Excel Datei",
                                           filetypes=(("Excel files", "*.xls"), ("Excel files", "*.xlsx")))
     try:
         DATA = pd.read_excel(FILEPATH)
@@ -25,59 +28,59 @@ def open_file_for_pd():
         calculate_worktime()
 
 
-def set_options():
-    global OPTION
-    if cb_all_var.get() == 1:
-        OPTION = 0
-        cb_bes_var.set(1)
-        cb_pen_var.set(1)
-    elif cb_pen_var.get() == 1 and cb_bes_var.get() == 1:
-        OPTION = 3
-        cb_all_var.set(0)
-    elif cb_bes_var.get() == 1:
-        OPTION = 1
-        cb_all_var.set(0)
-    elif cb_pen_var.get() == 1:
-        OPTION = 2
-        cb_all_var.set(0)
+def categorie_change(cat_string):
+    if cat_string in CATEGORIE:
+        CATEGORIE.remove(cat_string)
     else:
-        OPTION = -1
-    if FILEPATH is not None:
-        calculate_worktime()
+        CATEGORIE.append(cat_string)
+    calculate_worktime()
+
+
+def status_change(stat_string):
+    if stat_string in STATUS:
+        STATUS.remove(stat_string)
+    else:
+        STATUS.append(stat_string)
+    calculate_worktime()
+
+
+def filter_df_by_categories(df):
+    df['Kategorie'] = df['Kategorie'].fillna("Keine")
+    for index, row in df.iterrows():
+        if row["Kategorie"] in CATEGORIE:
+            pass
+        else:
+            df = df.drop(index)
+    return df
+
+
+def filter_df_by_status(df):
+    for index, row in df.iterrows():
+        if row["Status"] in STATUS:
+            pass
+        else:
+            df = df.drop(index)
+    return df
 
 
 def calculate_worktime():
-    hours = 0
-    minutes = 0
-    seconds = 0
+    global DATA
+    df = DATA
     try:
-        if OPTION == -1:
-            gui.worktime_label.config(text="00:00:00")
-            gui.path_label.config(text="In Bearbeitung: " + FILEPATH)
+        if FILEPATH is not None:
+            df = filter_df_by_categories(df)
+            df = filter_df_by_status(df)
+        else:
             return
-        if OPTION == 0:
-            for time in DATA["Arbeitszeit"]:
-                hours += int(time[0:2])
-                minutes += int(time[3:5])
-                seconds += int(time[6:8])
-        elif OPTION == 1:
-            for index, row in DATA.iterrows():
-                if row["Status"] == "Bestätigt":
-                    hours += int(row["Arbeitszeit"][0:2])
-                    minutes += int(row["Arbeitszeit"][3:5])
-                    seconds += int(row["Arbeitszeit"][6:8])
-        elif OPTION == 2:
-            for index, row in DATA.iterrows():
-                if row["Status"] == "Eingereicht":
-                    hours += int(row["Arbeitszeit"][0:2])
-                    minutes += int(row["Arbeitszeit"][3:5])
-                    seconds += int(row["Arbeitszeit"][6:8])
-        elif OPTION == 3:
-            for index, row in DATA.iterrows():
-                if row["Status"] == "Bestätigt" or row["Status"] == "Eingereicht":
-                    hours += int(row["Arbeitszeit"][0:2])
-                    minutes += int(row["Arbeitszeit"][3:5])
-                    seconds += int(row["Arbeitszeit"][6:8])
+        hours = 0
+        minutes = 0
+        seconds = 0
+
+        for time in df["Arbeitszeit"]:
+            hours += int(time[0:2])
+            minutes += int(time[3:5])
+            seconds += int(time[6:8])
+
         minutes += int(seconds / 60)
         hours += int(minutes / 60)
         minutes = minutes % 60
@@ -90,16 +93,41 @@ def calculate_worktime():
         gui.path_label.config(text="In Bearbeitung: " + FILEPATH)
 
 
-# -----MAIN-----
+# -----MAIN GUI-----
 gui = GUI_Setup()
-cb_all_var = IntVar()
-cb_bes_var = IntVar()
-cb_pen_var = IntVar()
+cb_bes_var = IntVar(value=1)
+cb_pen_var = IntVar(value=1)
+cb_vor_var = IntVar(value=0)
+cb_gel_var = IntVar(value=0)
+cat_ums_var = IntVar(value=1)
+cat_pro_var = IntVar(value=1)
+cat_auf_var = IntVar(value=1)
+cat_vot_var = IntVar(value=1)
+cat_hom_var = IntVar(value=0)
+cat_bes_var = IntVar(value=0)
+cat_bue_var = IntVar(value=0)
+cat_fah_var = IntVar(value=0)
+cat_mat_var = IntVar(value=0)
+cat_pau_var = IntVar(value=0)
+cat_pri_var = IntVar(value=0)
+cat_swe_var = IntVar(value=0)
+cat_nan_var = IntVar(value=1)
 gui.open_file_btn.config(command=lambda: open_file_for_pd())
-gui.all_cb.config(variable=cb_all_var, command=lambda: set_options())
-gui.bes_cb.config(variable=cb_bes_var, command=lambda: set_options())
-gui.pen_cb.config(variable=cb_pen_var, command=lambda: set_options())
-cb_all_var.set(0)
-cb_pen_var.set(1)
-cb_bes_var.set(1)
+gui.bes_cb.config(variable=cb_bes_var, command=lambda: status_change("Bestätigt"))
+gui.pen_cb.config(variable=cb_pen_var, command=lambda: status_change("Eingereicht"))
+gui.vor_cb.config(variable=cb_vor_var, command=lambda: status_change("Vorläufig"))
+gui.gel_cb.config(variable=cb_gel_var, command=lambda: status_change("Gelöscht"))
+gui.cat_cb_nan.config(variable=cat_nan_var, command=lambda: categorie_change("Keine"))
+gui.cat_cb_ums.config(variable=cat_ums_var, command=lambda: categorie_change("Umsetzung"))
+gui.cat_cb_pro.config(variable=cat_pro_var, command=lambda: categorie_change("Projektierung"))
+gui.cat_cb_auf.config(variable=cat_auf_var, command=lambda: categorie_change("Aufgabe"))
+gui.cat_cb_vot.config(variable=cat_vot_var, command=lambda: categorie_change("Vor-Ort-Termin"))
+gui.cat_cb_hom.config(variable=cat_hom_var, command=lambda: categorie_change("Homeoffice"))
+gui.cat_cb_bes.config(variable=cat_bes_var, command=lambda: categorie_change("Besprechung"))
+gui.cat_cb_bue.config(variable=cat_bue_var, command=lambda: categorie_change("Büro"))
+gui.cat_cb_fah.config(variable=cat_fah_var, command=lambda: categorie_change("Fahrtzeit"))
+gui.cat_cb_mat.config(variable=cat_mat_var, command=lambda: categorie_change("Materialfahrt"))
+gui.cat_cb_pau.config(variable=cat_pau_var, command=lambda: categorie_change("Pause"))
+gui.cat_cb_pri.config(variable=cat_pri_var, command=lambda: categorie_change("Privat"))
+gui.cat_cb_swe.config(variable=cat_swe_var, command=lambda: categorie_change("Schlechtwetter"))
 gui.root.mainloop()
--- ui.py
+++ ui.py
@@ -7,47 +7,107 @@ class GUI_Setup:
 
     def __init__(self):
         self.root = Tk()
-        self.root.config(width=500, height=800, bg=THEME_COLOR, pady=20, padx=20)
+        self.root.config(bg=THEME_COLOR, pady=20, padx=20)
+        self.root.minsize(500, 500)
         self.root.iconbitmap("data/hero.ico")
         self.root.title("Hero Timer")
 
         self.header_label = Label()
-        self.header_label.config(text="Hero Stundenzettel", pady=35, bg=THEME_COLOR,
-                                 font=("Arial", 18, "bold"), fg="#ffdd00")
-        self.header_label.grid(column=0, row=0)
+        self.header_label.config(text="Hero Stundenzettel", bg=THEME_COLOR, font=("Arial", 18, "bold"), fg="#ffdd00")
+        self.header_label.place(x=225, y=20, anchor=CENTER)
 
         self.open_file_btn = Button()
         self.open_file_btn.config(text="Open File", width=30)
-        self.open_file_btn.grid(column=0, row=1, pady=20)
+        self.open_file_btn.place(x=225, y=70, anchor=CENTER)
 
         self.state_label = Label()
-        self.state_label.config(text="Status Option", pady=5, bg=THEME_COLOR, font=("Arial", 10), fg="#ffdd00")
-        self.state_label.grid(column=0, row=2, sticky="W")
-
-        self.all_cb = Checkbutton()
-        self.all_cb.config(text="Alle inkl. gelöscht & vorläufig", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
-        self.all_cb.grid(column=0, row=3, pady=5, sticky="W")
+        self.state_label.config(text="Status Option", bg=THEME_COLOR, font=("Arial", 10), fg="#ffdd00")
+        self.state_label.place(x=10, y=140, anchor="w")
 
         self.bes_cb = Checkbutton()
         self.bes_cb.config(text="Bestätigt", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
-        self.bes_cb.grid(column=0, row=4, padx=00, sticky="W")
+        self.bes_cb.place(x=10, y=170, anchor="w")
 
         self.pen_cb = Checkbutton()
         self.pen_cb.config(text="Eingereicht", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
-        self.pen_cb.grid(column=0, row=4, padx=95, sticky="W")
+        self.pen_cb.place(x=110, y=170, anchor="w")
+
+        self.vor_cb = Checkbutton()
+        self.vor_cb.config(text="Vorläufig", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.vor_cb.place(x=210, y=170, anchor="w")
+
+        self.gel_cb = Checkbutton()
+        self.gel_cb.config(text="Gelöscht", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.gel_cb.place(x=310, y=170, anchor="w")
+
+        self.cat_label = Label()
+        self.cat_label.config(text="Kategorie Option", bg=THEME_COLOR, font=("Arial", 10), fg="#ffdd00")
+        self.cat_label.place(x=10, y=220, anchor="w")
+
+        self.cat_cb_nan = Checkbutton()
+        self.cat_cb_nan.config(text="Ohne Kategorie", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_nan.place(x=10, y=250, anchor="w")
+
+        self.cat_cb_ums = Checkbutton()
+        self.cat_cb_ums.config(text="Umsetzung", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_ums.place(x=10, y=280, anchor="w")
+
+        self.cat_cb_pro = Checkbutton()
+        self.cat_cb_pro.config(text="Projektierung", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_pro.place(x=110, y=280, anchor="w")
+
+        self.cat_cb_auf = Checkbutton()
+        self.cat_cb_auf.config(text="Aufgabe", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_auf.place(x=210, y=280, anchor="w")
+
+        self.cat_cb_vot = Checkbutton()
+        self.cat_cb_vot.config(text="Vor-Ort-Termin", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_vot.place(x=310, y=280, anchor="w")
+
+        self.cat_cb_bue = Checkbutton()
+        self.cat_cb_bue.config(text="Büro", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_bue.place(x=110, y=340, anchor="w")
+
+        self.cat_cb_fah = Checkbutton()
+        self.cat_cb_fah.config(text="Fahrzeit", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_fah.place(x=110, y=310, anchor="w")
+
+        self.cat_cb_bes = Checkbutton()
+        self.cat_cb_bes.config(text="Besprechung", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_bes.place(x=210, y=310, anchor="w")
+
+        self.cat_cb_hom = Checkbutton()
+        self.cat_cb_hom.config(text="Homeoffice", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_hom.place(x=310, y=310, anchor="w")
+
+        self.cat_cb_pau = Checkbutton()
+        self.cat_cb_pau.config(text="Pause", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_pau.place(x=10, y=340, anchor="w")
+
+        self.cat_cb_mat = Checkbutton()
+        self.cat_cb_mat.config(text="Materialfahrt", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_mat.place(x=10, y=310, anchor="w")
+
+        self.cat_cb_pri = Checkbutton()
+        self.cat_cb_pri.config(text="Privat", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_pri.place(x=210, y=340, anchor="w")
+
+        self.cat_cb_swe = Checkbutton()
+        self.cat_cb_swe.config(text="Schlechtwetter", bg=THEME_COLOR, highlightcolor=THEME_COLOR)
+        self.cat_cb_swe.place(x=310, y=340, anchor="w")
 
         self.worktime_info_label = Label()
         self.worktime_info_label.config(text="Arbeitszeit in Stunden:Minuten:Sekunden",
-                                        pady=10, bg=THEME_COLOR, font=("Arial", 15), fg="#ffdd00")
-        self.worktime_info_label.grid(column=0, row=5)
+                                        bg=THEME_COLOR, font=("Arial", 15), fg="#ffdd00")
+        self.worktime_info_label.place(x=225, y=380, anchor=CENTER)
 
         self.worktime_label = Label()
-        self.worktime_label.config(text="00:00:00", pady=5, bg=THEME_COLOR, font=("Arial", 15))
-        self.worktime_label.grid(column=0, row=6)
+        self.worktime_label.config(text="00:00:00", bg=THEME_COLOR, font=("Arial", 15))
+        self.worktime_label.place(x=225, y=420, anchor=CENTER)
 
         self.path_label = Label()
-        self.path_label.config(text="", pady=5, bg=THEME_COLOR, font=("Arial", 10))
-        self.path_label.grid(column=0, row=7)
+        self.path_label.config(text="", bg=THEME_COLOR, font=("Arial", 10))
+        self.path_label.place(x=225, y=100, anchor=CENTER)
 
 
 
